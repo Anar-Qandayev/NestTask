d17da3990f9d0cb9d4773de1c4c9b845e4afd80c
Anar-Qandayev <tu7bxfga8@code.edu.az>
2022-05-24 23:16:47 +0400

    commit

---
+++ Nest-Back-End/NestTask/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NestTask.Data_Access_Layer;
+using NestTask.HomeVM;
+using NestTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NestTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext _context { get; }
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            HomeVm homeVM = new HomeVm()
+            {
+
+                Sliders = await _context.Sliders.ToListAsync(),
+                Categories = await _context.Categories.ToListAsync(),
+                Products = await _context.Products.Include(p => p.ProductImages).Include(p => p.Category).Take(10).ToListAsync()
+
+            };
+
+            return View(homeVM);
+        }
+    }
+}
